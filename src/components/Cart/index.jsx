ae7b1ae43854e6c6b62b45f290b35ddb6e027d79
suffo99 <maxtimoshin94@gmail.com>
2022-07-27 13:23:11 +0300

    sevdf

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -4,7 +4,7 @@ import { clearCart, deleteProductFromCart } from '../../redux/Slices/cartSlice';
 import './style.css'
 
 const Cart = () => {
-    const [cartIsOpen , cartToggler] = useState(false)
+    const [cartIsOpen, cartToggler] = useState(false)
     const cartHandler = () => {
         cartToggler(cartIsOpen ? false : true)
     }
@@ -19,19 +19,19 @@ const Cart = () => {
     return (
         <>
             <div className="cart">
-                <div className="cart-icon" onClick={() => {cartHandler()}}>CART</div>
+                <div className="cart-icon" onClick={() => { cartHandler() }}></div>
                 <div className={cartIsOpen ? 'cart-bar visible' : 'cart-bar'}>
-                    <ul className='cart-items-list'>
+                    <ul className={cartProducts.length >= 5 ? 'cart-items-list cart-scroll' : 'cart-items-list'}>
                         {cartProducts.length !== 0 ? cartProducts.map(product => (
-                            <li className='cart-item' key={product.id}>{product.title}| {product.price}<button onClick={() => deleteHandler(product.id)}>delete</button></li>
-                        )) : 'Cart is empty'}
+                            <li className='cart-item' key={product.id}><div className="product-title">{product.title}</div><div className='product-price'> {product.price.toLocaleString("ru", { style: 'currency', currency: 'usd' })}</div><button className='cart-delete-button' onClick={() => deleteHandler(product.id)}>delete</button></li>
+                        )) : <div className='empty-cart'>Your cart is empty</div>}
                     </ul>
-                    <div className="sum">
+                    <div className="cart-summ">
                         {cartProducts.length !== 0 ? cartProducts.reduce((acc, item) => (
                             acc += item.price
-                        ), 0) : 0}
+                        ), 0).toLocaleString("ru", { style: 'currency', currency: 'usd' }) : 0}
                     </div>
-                    <button onClick={() => clearCartHandler()}>Clear Cart</button>
+                    <button className='clear-cart' onClick={() => clearCartHandler()}>Clear Cart</button>
                 </div>
             </div>
         </>
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -58,8 +58,6 @@ const MainPage = () => {
                     <option value="5">5</option>
                     <option value="10">10</option>
                 </select>
-
-                <Cart />
                 {/* <InputsList />
         <FiltersList /> */}
             </div>
